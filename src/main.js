2660c4f6c476122c2811d6db42a36286db09bc3b
bohong <977106024@qq.com>
2019-04-15 18:58:29 +0800

    关闭vue代码规范检测 引入flyio

--- src/main.js
+++ src/main.js
@@ -4,5 +4,8 @@ import App from './App'
 Vue.config.productionTip = false
 App.mpType = 'app'
 
+let Fly=require("flyio/dist/npm/wx");
+let fly=new Fly;	//实例化
+Vue.prototype.$http=fly //将fly实例挂在vue原型上
 const app = new Vue(App)
 app.$mount()
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -15,71 +15,57 @@
 </template>
 
 <script>
-import card from '@/components/card'
-
 export default {
   data () {
     return {
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
-    }
+      
+    };
   },
 
-  components: {
-    card
+  created() {
+    this.$http
+      .get("http://api.xuewuzhijing.top:2333/weChatApp/noteList")
+      .then(res => {
+        console.log(res);
+      });
   },
+
+  components: {},
 
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
-    },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
-    }
+    
   },
 
-  created () {
-    // let app = getApp()
-  }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-  #note{
-    ul{
-      padding: 50px 25px;
-      li{
-        // background: #e98d3f;
-        border: 1px solid orange;
-        padding: 5px 10px;
-        margin-bottom: 15px;
-        p{
-          &:first-child{
-            border-bottom: 1px solid #ccc;
-          }
+#note {
+  ul {
+    padding: 50px 25px;
+    li {
+      // background: #e98d3f;
+      border: 1px solid #ccc;
+      padding: 5px 10px;
+      margin-bottom: 15px;
+      p {
+        &:first-child {
+          border-bottom: 1px solid #ccc;
         }
       }
     }
   }
-  footer{
-    position: fixed;
-    bottom: 30px;
-    left: 50%;
-    transform: translateX(-50%);
-    button{
-      height: 60px;
-      width: 60px;
-      border-radius: 50px;
-      border: none;
-    }
+}
+footer {
+  position: fixed;
+  bottom: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+  button {
+    height: 60px;
+    width: 60px;
+    border-radius: 50px;
+    border: none;
   }
+}
 </style>
